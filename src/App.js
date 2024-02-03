f6f711e00262d6c9454d24816d671ced2cecb579
Niru1pam2 <nirupamtalukdar123@gmail.com>
2024-02-03 22:37:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
+import Services from "./components/Services";
+import Tours from "./components/Tours";
+import Navbar from "./components/navbar";
+
+
+
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <Hero/>
+   <About/>
+   <Services/>
+   <Tours/>
+   <Footer/>
+
+  </>
+  )
+}
+
+export default App;
